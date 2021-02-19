6e136125814d74a794e6534e79b8d74edd5555b8
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2021-02-19 16:52:00 +0100

    ci: add prettier, precommit and prepush hooks

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,16 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+/**
+ * fix: `matchMedia` not present, legacy browsers require a polyfill
+ */
+global.matchMedia =
+    global.matchMedia ||
+    function () {
+        return {
+            matches: false,
+            addListener: function () {},
+            removeListener: function () {},
+        };
+    };
